18939bcb8b3d272dc1a80e0e547b9a72ee8fd31b
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-02 21:09:18 -0300

    empty methods for AuthorController

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Return the list of authors;
+     * 
+     * @return Response
+     */
+    public function index(): Response
+    {
+
+    }
+
+    /**
+     * Create a new author;
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+
+    }
+
+    /**
+     * Obtains and show an author;
+     *
+     * @param integer $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+
+    }
+
+    /**
+     * Updates an author;
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return Response
+     */
+    public function update(Request $request, int $id): Response
+    {
+
+    }
+
+    /**
+     * Removes an author;
+     *
+     * @param integer $id
+     * @return Response
+     */
+    public function destroy(int $id): Response
+    {
+
+    }
+}
